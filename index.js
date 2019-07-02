7b258ad6d9f35d0aa8e424bd46987c04d30f0c0c
ninganagoud-vk <52402472+ninganagoud-vk@users.noreply.github.com>
2019-07-02 12:54:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 document.getElementById('sample').innerHTML= "hey is from javascript "
+document.getElementById('sample').innerHTML= "hey is from javascript1a "
+
+document.getElementById('sample').innerHTML= "hey is from javascript1b"
