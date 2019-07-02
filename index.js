d1abf67fffba559ad0c6a36b5dac4a68583a72db
ninganagoud-vk <52402472+ninganagoud-vk@users.noreply.github.com>
2019-07-02 12:57:18 +0530

    changes 4th time in js

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ document.getElementById('sample').innerHTML= "hey is from javascript "
 document.getElementById('sample').innerHTML= "hey is from javascript1a "
 
 document.getElementById('sample').innerHTML= "hey is from javascript1b"
+document.getElementById('sample').innerHTML= "hey is from javascript "
+
